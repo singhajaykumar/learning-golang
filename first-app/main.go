1814c78fa3776eea7bb68cd655198208713a2a92
Ajay Singh <aks.vsingh@gmail.com>
2025-02-14 22:48:55 +0530

    added comments in first app

--- first-app/main.go
+++ first-app/main.go
@@ -1,9 +1,17 @@
+// In a Go application, all code is organized into packages.
+// The first line specifies the package name.
 package main
 
+// The fmt package is used for formatted input and output operations.
 import "fmt"
 
+// The main function is the entry point of a Go application.
+// Every Go application has a single entry point.
 func main() {
 
-	fmt.Println("Hello World")
+	fmt.Println("Hello World") // Prints "Hello World" to the console.
 
 }
+
+// To execute a Go application, use the following command:
+// --> go run <application-name>
